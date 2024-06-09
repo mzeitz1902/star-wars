6c4a36aabbb9b02740714cf9fb0801c2064db2e0
AlcuZan <manuel_zeitz@outlook.de>
2024-06-09 22:13:04 +0200

    component tests

--- src/app/pages/people/components/people/content/content.component.cy.ts
+++ src/app/pages/people/components/people/content/content.component.cy.ts
@@ -1,18 +1,47 @@
-import { Component } from '@angular/core';
 import { MountConfig } from 'cypress/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { cy } from 'local-cypress';
+import { ContentComponent } from './content.component';
+import { MockProvider } from 'ng-mocks';
+import { PeopleViewService } from '../people-view.service';
+import { signal } from '@angular/core';
+import { Person } from '../../../person.interface';
+import { DeferBlockState } from '@angular/core/testing';
 
-describe(``, () => {
-  let config: MountConfig<WrapperComponent>;
-  it(`should `, () => {});
+describe(`ContentComponent`, () => {
+  let config: MountConfig<ContentComponent>;
+  it(`should not show content when 'isLoading' is true`, () => {
+    mount(true);
+    cy.get('app-content').should('not.exist');
+  });
+  it('should show list of people', () => {
+    mount(false).defer().renderAll(DeferBlockState.Complete);
+    cy.get('app-people-list').should('exist');
+  });
+  it('should show selected person details', () => {
+    const selectedPerson = { id: '1', name: 'John' } as Person;
+    mount(false, selectedPerson).defer().renderAll(DeferBlockState.Complete);
+    cy.get('router-outlet').should('exist');
+  });
 
-  function mount() {
+  function mount(isLoading: boolean, selectedPerson?: Person) {
+    const fakePeople = [
+      { id: '1', name: 'John' },
+      { id: '2', name: 'Jane' },
+    ] as Person[];
     config = {
       imports: [BrowserAnimationsModule],
+      providers: [
+        MockProvider(PeopleViewService, {
+          isLoading: signal(isLoading),
+          selectedPerson: signal(selectedPerson),
+          people: signal(fakePeople),
+          count: signal(0),
+          pageSize: signal(0),
+          currentPage: signal(0),
+        }),
+      ],
     };
-    cy.mount(WrapperComponent, config);
+    return cy.mount(ContentComponent, config);
   }
 });
-
-@Component({ template: `` })
-class WrapperComponent {}
